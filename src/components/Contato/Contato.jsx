99b78d05dff18ff8ac8cac1bf0db859bb714cd76
marcusvmota <marcusmota08@gmail.com>
2023-05-07 19:50:23 -0300

    portolio refatorado react

--- src/components/Contato/Contato.jsx
+++ src/components/Contato/Contato.jsx
@@ -1,10 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Contato.css"
 
 const Contato = () => {
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [telefone, setTelefone] = useState("")
+  const [mensagem, setMensagem] = useState("")
+
+  const enviarFormulario = (event) => {
+    event.preventDefault()
+    console.log("Formulário enviado com sucesso!")
+
+    const texto = `Nome: ${nome} \nEmail: ${email} \nTelefone: ${telefone} \nMensagem: ${mensagem}`
+    const numeroWhatsapp = import.meta.env.VITE_WHATSAPP_NUMBER
+    const linkWhatsapp = `https://wa.me/${numeroWhatsapp}?text=${encodeURIComponent(
+      texto
+    )}`
+    window.open(linkWhatsapp, "_blank")
+  }
+
+  const mascaraTelefone = (event) => {
+    const texto = event.target.value
+    const textoApenasNumeros = texto.replace(/\D/g, "").substring(0, 11)
+
+    let telefoneFormatado = textoApenasNumeros.replace(
+      /^(\d{2})(\d{5})(\d{4)/g,
+      "($1) $2"
+    )
+
+    if (telefoneFormatado.length > 11) {
+      telefoneFormatado = textoApenasNumeros.replace(
+        /^(\d{2})(\d{4})(\d{0,4})/,
+        "($1) $2 -$3"
+      )
+    }
+    setTelefone(telefoneFormatado)
+  }
+
   return (
     <>
-      <form onSubmit="event.preventDefault()">
+      <form onSubmit={enviarFormulario}>
         <fieldset>
           <label htmlFor="input-nome">Nome</label>
           <input
@@ -13,12 +48,21 @@ const Contato = () => {
             id="input-nome"
             required
             minLength="2"
+            value={nome}
+            onChange={(event) => setNome(event.target.value)}
           />
         </fieldset>
 
         <fieldset>
           <label htmlFor="input-email">E-mail</label>
-          <input type="email" name="input-email" id="input-email" required />
+          <input
+            type="email"
+            name="input-email"
+            id="input-email"
+            required
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </fieldset>
 
         <fieldset>
@@ -31,6 +75,8 @@ const Contato = () => {
             required
             pattern="^\(\d{2}\) \d{5}-\d{4}$"
             maxLength="15"
+            value={telefone}
+            onChange={mascaraTelefone}
           />
         </fieldset>
 
@@ -41,16 +87,13 @@ const Contato = () => {
             id="input-msg"
             cols="30"
             rows="10"
+            value={mensagem}
+            onChange={(event) => setMensagem(event.target.value)}
           ></textarea>
         </fieldset>
 
         <center>
-          <input
-            type="submit"
-            value="ENVIAR"
-            className="button"
-            onClick="enviarhtmlFormulario()"
-          />
+          <input type="submit" value="ENVIAR" className="button" />
         </center>
       </form>
     </>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,36 +1,17 @@
 import React from "react"
 import "./Portfolio.css"
 import ItemPortfolio from "../ItemPortfolio/ItemPortfolio"
+import portfolio from "./Portfolio.json"
 
 const Portfolio = () => {
-  const portfolios = [
-    {
-      link: "https://academy.comeialabs.com.br/",
-      image: "https://picsum.photos/200/300?random=1",
-      title: "Card Interativo",
-    },
-
-    {
-      link: "https://academy.comeialabs.com.br/",
-      image: "https://picsum.photos/200/300?random=2",
-      title: "LandingPage",
-    },
-
-    {
-      link: "https://academy.comeialabs.com.br/",
-      image: "https://picsum.photos/200/300?random=3",
-      title: "Quadroa4",
-    },
-  ]
-
   return (
     <main>
-      {portfolios.map((portfolio, index) => (
+      {portfolio.map((item, index) => (
         <ItemPortfolio
           key={index}
-          link={portfolio.link}
-          image={portfolio.image}
-          title={portfolio.title}
+          link={item.link}
+          image={item.image + (index + 1)}
+          title={item.title}
         ></ItemPortfolio>
       ))}
     </main>
